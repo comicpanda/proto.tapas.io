df7ce9d36fe28f172481c853ef81f921b1f106bf
YoonKim <isyoon@tapasmedia.co>
2018-03-07 17:16:52 -0800

    Init

---
+++ routes/apps.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/home', (req, res, next) => {
+  res.render('apps/home', {menu: 'Tapas'});
+});
+
+router.get('/discovery', (req, res, next) => {
+  res.render('apps/discovery', {menu: 'Discover'});
+});
+
+router.get('/library', (req, res, next) => {
+  res.render('apps/library', {menu: 'Library'});
+});
+
+router.get('/inbox', (req, res, next) => {
+  res.render('apps/inbox', {menu: 'Inbox'});
+});
+
+module.exports = router;
